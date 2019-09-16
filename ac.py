1543f59ada3aa5f1ac07f91ae712a10d9349201f
Andreas Vrålstad <andreas.vralstad@gmail.com>
2019-09-16 12:42:14 +0200

    Split train and test into separate functions

--- ac.py
+++ ac.py
@@ -81,82 +81,79 @@ def create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num
         g['dataset_data'][idx] = melspec_normalized
         g['dataset_label'][idx] = label_to_target[label]
 
-def train(path_out, label_id_to_label, num_targets, num_epochs):
-  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-  net = ConvNet(num_targets, device).to(device)
+def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, batch_size):
   optimizer = optim.Adam(net.parameters(), lr=1e-3, weight_decay=1e-5)
-  with h5py.File(path_out,'r') as f:
-    dataset_train_data = f['train']['output_data']
-    dataset_train_label = f['train']['output_label']
-    dataset_test_data = f['test']['output_data']
-    dataset_test_label = f['test']['output_label']
-    for idx in range(5):
-      label_one_hot = dataset_train_label[idx]
-      label_id, = np.where(dataset_train_label[idx]==1)
-      label = label_id_to_label[label_id[0]]
-      # print(f'OneHot: {dataset_train_label[idx]} ID: {label_id} Label: {label}')
-    samples_train = dataset_train_data[()]
-    # Add an extra dimension (channel dimension) required by the CNN
-    samples_train = np.expand_dims(samples_train, axis=1)
-    samples_train = torch.from_numpy(samples_train).float()
-    samples_test = dataset_test_data[()]
-    samples_test = np.expand_dims(samples_test, axis=1)
-    samples_test = torch.from_numpy(samples_test).float()
-    #audio.print_melspectrogram(samples[0][0], 44100, 512, 20, 8000)
-    #plt.imshow(dataset_train_data[0],aspect='auto',origin='lower')
-    targets_train = dataset_train_label[()]
-    targets_train = torch.from_numpy(targets_train).long()
-    targets_train = torch.argmax(targets_train, 1)
-    targets_test = dataset_test_label[()]
-    targets_test = torch.from_numpy(targets_test).long()
-    targets_test = torch.argmax(targets_test, 1)
-    lossfn = nn.CrossEntropyLoss()
-    net.train()
-    batch_size = 1000
-    num_batches = (len(samples_train)-1)//batch_size+1
-    print('Num batches:',num_batches)
-    for idx_epoch in range(num_epochs):
-      running_loss = 0
-      for idx_batch in range(num_batches):
-        start = idx_batch*batch_size
-        end = (idx_batch+1)*batch_size
-        samples = samples_train[start:end]
-        targets = targets_train[start:end]
-        optimizer.zero_grad()
-        output = net(samples)
-        loss = lossfn(output, targets)
-        loss.backward()
-        optimizer.step()
-        print('*',end='',flush=True)
-        running_loss += loss.item()
-      print(f'\nDone with epoch {idx_epoch+1}/{num_epochs}. Loss: {running_loss/num_batches:.5f}')
-    num_batches = (len(samples_test)-1)//batch_size+1
-    with torch.no_grad():        
-      target_total = np.zeros(num_targets)
-      target_correct = np.zeros(num_targets)
-      for idx_batch in range(num_batches):
-        start = idx_batch*batch_size
-        end = (idx_batch+1)*batch_size
-        samples = samples_test[start:end]
-        targets = targets_test[start:end]
-        output = net(samples)
-        output = torch.argmax(output, 1)
-        for idx in range(len(output)):
-          target_total[targets[idx]] += 1
-          if targets[idx]==output[idx]:
-            target_correct[targets[idx]] +=1
-      for idx in range(num_targets):
-        label = label_id_to_label[idx]
-        percent = 100*target_correct[idx]/np.maximum(target_total[idx],1)
-        print(f'{label:>25}: {percent:.2f}%')
-      tot_all = np.sum(target_total)
-      tot_correct = np.sum(target_correct)
-      tot = 100*tot_correct/tot_all
-      print(f'Total: {tot:.2f}% ({tot_correct:.0f}/{tot_all:.0f})')
+  samples_train = dataset_train_data[()]
+  # Add an extra dimension (channel dimension) required by the CNN
+  samples_train = np.expand_dims(samples_train, axis=1)
+  samples_train = torch.from_numpy(samples_train).float()
+  #audio.print_melspectrogram(samples[0][0], 44100, 512, 20, 8000)
+  #plt.imshow(dataset_train_data[0],aspect='auto',origin='lower')
+  targets_train = dataset_train_label[()]
+  targets_train = torch.from_numpy(targets_train).long()
+  targets_train = torch.argmax(targets_train, 1)
+  lossfn = nn.CrossEntropyLoss()
+  net.train()
+  num_batches = (len(samples_train)-1)//batch_size+1
+  print('Num batches:',num_batches)
+  for idx_epoch in range(num_epochs):
+    running_loss = 0
+    for idx_batch in range(num_batches):
+      start = idx_batch*batch_size
+      end = (idx_batch+1)*batch_size
+      samples = samples_train[start:end]
+      targets = targets_train[start:end]
+      optimizer.zero_grad()
+      output = net(samples)
+      loss = lossfn(output, targets)
+      loss.backward()
+      optimizer.step()
+      print('*',end='',flush=True)
+      running_loss += loss.item()
+    print(f'\nDone with epoch {idx_epoch+1}/{num_epochs}. Loss: {running_loss/num_batches:.5f}')
+
+
+def test(net, dataset_test_data, dataset_test_label, label_id_to_label, batch_size):
+  samples_test = dataset_test_data[()]
+  samples_test = np.expand_dims(samples_test, axis=1)
+  samples_test = torch.from_numpy(samples_test).float()
+  targets_test = dataset_test_label[()]
+  targets_test = torch.from_numpy(targets_test).long()
+  targets_test = torch.argmax(targets_test, 1)
+  for idx in range(5):
+    label_one_hot = dataset_train_label[idx]
+    label_id, = np.where(dataset_train_label[idx]==1)
+    label = label_id_to_label[label_id[0]]
+    # print(f'OneHot: {dataset_train_label[idx]} ID: {label_id} Label: {label}')
+  num_batches = (len(samples_test)-1)//batch_size+1
+  net.eval()
+  with torch.no_grad():        
+    target_total = np.zeros(num_targets)
+    target_correct = np.zeros(num_targets)
+    for idx_batch in range(num_batches):
+      start = idx_batch*batch_size
+      end = (idx_batch+1)*batch_size
+      samples = samples_test[start:end]
+      targets = targets_test[start:end]
+      output = net(samples)
+      output = torch.argmax(output, 1)
+      for idx in range(len(output)):
+        target_total[targets[idx]] += 1
+        if targets[idx]==output[idx]:
+          target_correct[targets[idx]] +=1
+    for idx in range(num_targets):
+      label = label_id_to_label[idx]
+      percent = 100*target_correct[idx]/np.maximum(target_total[idx],1)
+      print(f'{label:>25}: {percent:.2f}%')
+    tot_all = np.sum(target_total)
+    tot_correct = np.sum(target_correct)
+    tot = 100*tot_correct/tot_all
+    print(f'Total: {tot:.2f}% ({tot_correct:.0f}/{tot_all:.0f})')
 
 def parse_command_line():
   parser = argparse.ArgumentParser()
   parser.add_argument("--num_epochs", "-e", default=20, type=int, required=False, help="Number of epochs")
+  parser.add_argument("--batch_size", "-bs", default=1000, type=int, required=False, help="Batch size")
   parser.add_argument("--num_samples", "-n", default=np.inf, type=int, required=False, help="Number of samples to use for training and testing. NUM_SAMPLES = min(NUM_SAMPLES, actual number of samples)")
   args = parser.parse_args()
   return args
@@ -181,14 +178,22 @@ if __name__=='__main__':
   num_samples_test = int(np.minimum(num_samples_test, args.num_samples))
   num_epochs = args.num_epochs
 
-  print(f'Number of train samples: {num_samples_train}')
-  
   before = time.time()
-
+  
+  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+  net = ConvNet(num_targets, device).to(device)
+  print(f'Number of train samples: {num_samples_train}')
+  print(f'Hardware used for processing: {device}')
   setup_output(path_out, labels_train, num_samples_train, num_samples_test, num_targets)
   create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num_samples_test, labels_train, labels_test)
   print('Start training')
-  train(path_out, label_id_to_label, num_targets, num_epochs)
+  with h5py.File(path_out,'r') as f:
+    dataset_train_data = f['train']['output_data']
+    dataset_train_label = f['train']['output_label']
+    dataset_test_data = f['test']['output_data']
+    dataset_test_label = f['test']['output_label']
+    train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, args.batch_size)
+    test(net, dataset_test_data, dataset_test_label, label_id_to_label, args.batch_size)
 
   after = time.time()
   t_tot = after-before

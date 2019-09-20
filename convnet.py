bf917b43d7cd4c9ba0ef29677700467cdbcf7a3b
Andreas Vrålstad <andreas.vralstad@gmail.com>
2019-09-20 13:01:56 +0200

    Dropout --> Batch normalization for the conv layers

--- convnet.py
+++ convnet.py
@@ -26,18 +26,18 @@ class ConvNet(nn.Module):
         self.__num_classes = num_classes
         # Expected input shape: 64x259
         self.encoder = nn.Sequential(
-            nn.Dropout2d(p=0.1),
             nn.Conv2d(1, 32, 2, stride=2), # Result: 32 channels of size 32x129
-            nn.ReLU(True), # Perform ReLU in-place
-            nn.Dropout2d(p=0.2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(), # Perform ReLU in-place
             nn.Conv2d(32, 64, 2, stride=2), # Result: 64 channels of size 16x64
-            nn.ReLU(True),
-            nn.Dropout2d(p=0.2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.Conv2d(64, 128, 2, stride=2), # Result: 128 channels of size 8x32
-            nn.ReLU(True),
-            nn.Dropout2d(p=0.2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
             ReshapeDynamic((-1, 1, 128*8*32)),
             nn.Linear(128*8*32, num_classes).to(processing_device),
+            nn.Dropout(0.5),
             #PrintLayer('Before softmax'),
             nn.Softmax(dim=2)
             )

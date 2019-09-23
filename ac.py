d5320bdc0d5c93acdfd8ad8ae181089fcd8cb24c
Andreas Vrålstad <andreas.vralstad@gmail.com>
2019-09-23 17:23:18 +0200

    Fixed bug where data file where reset on every run

--- ac.py
+++ ac.py
@@ -29,20 +29,18 @@ def check_key_input():
     print('Breaking execution after current epoch', end='', flush=True)
   stop_training = True
 
-def setup_output(path_out, labels_train, num_samples_train, num_samples_test, num_targets):
+def setup_output(path_out, labels_train, num_samples_train, num_samples_test):
   # Load one of the WAV files so we can generate a melspectrogram and get the shape from there
   label = labels_train.iloc[0]['label']
   fname = labels_train.iloc[0]['fname']
   path = os.path.join(dir_train, fname)
   melspec, _ = audio.melspectrogram_from_file(path, label)
   groups = []
-  # Targets (labels) are one-hot encoded
-  # Our ConvNet need the channels dimension. It's always 1 for our melspecs
-  num_channels = 1
+  # Labels are integers, ranging from zero to (number of labels-1)
   groups.append(storage.HDF5Dataset('train', 'output_data', (num_samples_train, *melspec.shape), 'f'))
-  groups.append(storage.HDF5Dataset('train', 'output_label', (num_samples_train, num_targets), 'i'))
+  groups.append(storage.HDF5Dataset('train', 'output_label', (num_samples_train,), 'i'))
   groups.append(storage.HDF5Dataset('test', 'output_data', (num_samples_test, *melspec.shape), 'f'))
-  groups.append(storage.HDF5Dataset('test', 'output_label', (num_samples_test,num_targets), 'i'))
+  groups.append(storage.HDF5Dataset('test', 'output_label', (num_samples_test,), 'i'))
   storage.create_hdf5(path_out, groups)
 
 def setup_labels(path_labels_train, path_labels_test, only_manually_verified):
@@ -52,27 +50,23 @@ def setup_labels(path_labels_train, path_labels_test, only_manually_verified):
   if only_manually_verified==True:
     labels_train = labels_train[labels_train['manually_verified']==1]
   labels_test = pd.read_csv(path_labels_test)
-  label_to_label_id = {}
   label_id_to_label = {}
-  label_id_to_target = {}
-  label_to_target = {}
+  label_to_label_id = {}
   unique_labels = list(set(labels_train['label']))
-  for idx, l in enumerate(unique_labels):
-    label_to_label_id[l] = idx
-    label_id_to_label[idx] = l
-    target = np.zeros(len(unique_labels))
-    target[idx] = 1
-    label_id_to_target[idx] = target
-    label_to_target[l] = target
+  for idx, label in enumerate(unique_labels):
+    label_to_label_id[label] = idx
+    label_id_to_label[idx] = label
 
-  return labels_train, labels_test, label_to_label_id, label_id_to_label, label_id_to_target, label_to_target
+  return labels_train, labels_test, label_id_to_label, label_to_label_id
 
-def create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num_samples_test, labels_train, labels_test):
+def create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num_samples_test, labels_train, labels_test, label_to_label_id):
+  setup_output(path_out, labels_train, num_samples_train, num_samples_test)
   with h5py.File(path_out, 'r+') as f:
     dataset_data_train = f['train']['output_data']
     dataset_label_train = f['train']['output_label']
     dataset_data_test = f['test']['output_data']
     dataset_label_test = f['test']['output_label']
+    f.attrs['label_to_label_id'] = ','.join([(str(k)+':'+str(v)) for k, v in label_to_label_id.items()])
     group_data = []
     group_data.append({
       'path': dir_train,
@@ -94,12 +88,11 @@ def create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num
         fname = g['labels'].iloc[idx]['fname']
         path = os.path.join(g['path'], fname)
         melspec, sample_rate = audio.melspectrogram_from_file(path, label)
-        #melspec_normalized = utils.normalize(melspec)
         # if idx==0:
         #   audio.print_melspectrogram(melspec_normalized, sample_rate, 512, 20, 8000)
         # Store data to the first (index 0) channel
         g['dataset_data'][idx] = melspec
-        g['dataset_label'][idx] = label_to_target[label]
+        g['dataset_label'][idx] = label_to_label_id[label]
       # NB Make sure the train dataset is first, so we can perform normalization on that dataset
       if std==None:
         mean = np.mean(g['dataset_data'])
@@ -109,7 +102,7 @@ def create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num
       std2 = np.std(g['dataset_data'])
       print(f'Mean: {mean} std: {std} Mean2: {mean2} std2: {std2}')
 
-def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, batch_size):
+def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, batch_size, device):
   global stop_training
   optimizer = optim.Adam(net.parameters(), lr=1e-3, weight_decay=1e-5)
   samples_train = dataset_train_data[()]
@@ -120,7 +113,6 @@ def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs,
   #plt.imshow(dataset_train_data[0],aspect='auto',origin='lower')
   targets_train = dataset_train_label[()]
   targets_train = torch.from_numpy(targets_train).long()
-  targets_train = torch.argmax(targets_train, 1)
   lossfn = nn.CrossEntropyLoss()
   net.train()
   num_batches = (len(samples_train)-1)//batch_size+1
@@ -130,8 +122,8 @@ def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs,
     for idx_batch in range(num_batches):
       start = idx_batch*batch_size
       end = (idx_batch+1)*batch_size
-      samples = samples_train[start:end]
-      targets = targets_train[start:end]
+      samples = samples_train[start:end].to(device)
+      targets = targets_train[start:end].to(device)
       optimizer.zero_grad()
       output = net(samples)
       loss = lossfn(output, targets)
@@ -144,13 +136,12 @@ def train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs,
       break
 
 
-def test(net, dataset_test_data, dataset_test_label, label_id_to_label, batch_size):
+def test(net, dataset_test_data, dataset_test_label, label_id_to_label, batch_size, device):
   samples_test = dataset_test_data[()]
   samples_test = np.expand_dims(samples_test, axis=1)
   samples_test = torch.from_numpy(samples_test).float()
   targets_test = dataset_test_label[()]
   targets_test = torch.from_numpy(targets_test).long()
-  targets_test = torch.argmax(targets_test, 1)
   num_batches = (len(samples_test)-1)//batch_size+1
   net.eval()
   with torch.no_grad():        
@@ -159,8 +150,8 @@ def test(net, dataset_test_data, dataset_test_label, label_id_to_label, batch_si
     for idx_batch in range(num_batches):
       start = idx_batch*batch_size
       end = (idx_batch+1)*batch_size
-      samples = samples_test[start:end]
-      targets = targets_test[start:end]
+      samples = samples_test[start:end].to(device)
+      targets = targets_test[start:end].to(device)
       output = net(samples)
       output = torch.argmax(output, 1)
       for idx in range(len(output)):
@@ -195,27 +186,17 @@ if __name__=='__main__':
   path_labels_train = os.path.join(root, 'input/labels/train_post_competition.csv')
   path_labels_test = os.path.join(root, 'input/labels/test_post_competition_scoring_clips.csv')
   path_out = os.path.join(dir_out, 'out.hdf5')
-
-  utils.ensure_directory_existance([dir_out])
-  labels_train, labels_test, label_to_label_id, label_id_to_label, label_id_to_target, label_to_target = setup_labels(path_labels_train, path_labels_test, not args.include_all)
-
-  num_samples_train = labels_train.shape[0]
-  num_samples_test = labels_test.shape[0]
-  num_targets = len(label_to_target.keys())
-  num_samples_train = int(np.minimum(num_samples_train, args.num_samples))
-  num_samples_test = int(np.minimum(num_samples_test, args.num_samples))
-  num_epochs = args.num_epochs
-
   before = time.time()
-  
-  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-  net = ConvNet(num_targets, device).to(device)
-  print(f'Number of train samples: {num_samples_train}')
-  print(f'Hardware used for processing: {device}')
-  setup_output(path_out, labels_train, num_samples_train, num_samples_test, num_targets)
+
   if not os.path.exists(path_out) or args.rerun:
     print('Start generating melspectrograms')
-    create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num_samples_test, labels_train, labels_test)
+    utils.ensure_directory_existance([dir_out])
+    labels_train, labels_test, label_id_to_label, label_to_label_id = setup_labels(path_labels_train, path_labels_test, not args.include_all)
+    num_samples_train = labels_train.shape[0]
+    num_samples_test = labels_test.shape[0]
+    num_samples_train = int(np.minimum(num_samples_train, args.num_samples))
+    num_samples_test = int(np.minimum(num_samples_test, args.num_samples))
+    create_melspectrograms(path_out, dir_train, dir_test, num_samples_train, num_samples_test, labels_train, labels_test, label_to_label_id)
   else:
     print('Found existing melspectrograms')
   print('Start training')
@@ -226,14 +207,23 @@ if __name__=='__main__':
     dataset_train_label = f['train']['output_label']
     dataset_test_data = f['test']['output_data']
     dataset_test_label = f['test']['output_label']
-    train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, args.batch_size)
-    test(net, dataset_test_data, dataset_test_label, label_id_to_label, args.batch_size)
+    label_to_label_id = f.attrs['label_to_label_id']
+    label_to_label_id = label_to_label_id.split(',')
+    label_to_label_id = dict(x.split(':') for x in label_to_label_id)
+    num_samples_train = len(dataset_train_data)
+    label_id_to_label = inv_map = {int(v): k for k, v in label_to_label_id.items()}
+    num_targets = len(label_id_to_label.keys())
+    num_epochs = args.num_epochs
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print(f'Hardware used for processing: {device}')
+    net = ConvNet(num_targets, device).to(device)
+    train(net, dataset_train_data, dataset_train_label, num_targets, num_epochs, args.batch_size, device)
+    test(net, dataset_test_data, dataset_test_label, label_id_to_label, args.batch_size, device)
 
   after = time.time()
   t_tot = after-before
   t_avg = t_tot/num_samples_train
-  t_est = t_avg * labels_train.shape[0]
-  print(f'Time: {t_tot:.2f}s Avg: {t_avg:.2f}s Est: {t_est:.2f}s')
+  print(f'Time: {t_tot:.2f}s Avg: {t_avg:.2f}s')
   done = True
   if stop_training == False:
     print("Press Enter to quit")
